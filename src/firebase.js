4d25d398d6d4cebafc2069fb5eed6c84e3d8791c
Onkar2507 <onkarmarkad2507@gmail.com>
2026-07-23 01:02:55 +0530

    Clean up project and move Firebase config to environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAhOc--zv3y50T0Wy3p08N0TQD1RJ4lHlM",
-  authDomain: "collabkart-7aef1.firebaseapp.com",
-  projectId: "collabkart-7aef1",
-  storageBucket: "collabkart-7aef1.firebasestorage.app",
-  messagingSenderId: "985688679059",
-  appId: "1:985688679059:web:ac906dc5ea6fae2ddb6366"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
